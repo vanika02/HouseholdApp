a7cc0d0f63e5f43c5bf644b339dc7cc903cd06c9
vanika02 <vanikadangi02@gmail.com>
2026-06-21 09:55:56 +0000

    refactor: db and admin creation moved to main module

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from app import app
+from app.extensions import db
+from app.models import User
+from werkzeug.security import generate_password_hash
 from app.admin.routes import admin_router
 
 
@@ -6,6 +10,24 @@ from app.admin.routes import admin_router
 app=Flask(__name__)
 
 app.register_blueprint(admin_router)
+
+with app.app_context():
+    db.create_all()
+
+    admin = User.query.filter_by(role="admin").first()
+
+    if not admin:
+        password_hash = generate_password_hash("admin")
+        admin = User(
+            username="admin",
+            passhash=password_hash,
+            role="admin",
+            is_blocked=False,
+            is_verified=True
+        )
+        db.session.add(admin)
+        db.session.commit()
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
