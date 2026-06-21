36896fe55edbaa7f0e48156915320e8efd8d1259
vanika02 <vanikadangi02@gmail.com>
2026-06-21 02:43:04 +0000

    Refactor auth_required decorator into admin routes module

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,5 +1,16 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, render_template, abort, flash, session, redirect, url_for
+from functools import wraps
 
 admin_router = Blueprint("/admin", __name__, url_prefix="/admin")
+
+# decorator for auth function
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else:
+            flash('Please login to continue')
+            return redirect(url_for('login'))
+    return inner
 
