e8b75b77fb0937623b355983caa67469b61f42ab
vanika02 <vanikadangi02@gmail.com>
2026-06-23 16:36:27 +0000

    Refactor: configurations assign

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,11 @@ def create_app():
 
     app = Flask(__name__)
 
+    app.config['SECRET_KEY'] = Config.SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
+    app.config['UPLOAD_FOLDER'] = Config.UPLOAD_FOLDER
+    app.config['DEBUG'] = Config.DEBUG
 
     return app 
 
