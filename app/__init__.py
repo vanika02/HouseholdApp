7cb6f3828c73e1e7f2476f33e8bc93e37cc8e375
vanika02 <vanikadangi02@gmail.com>
2026-06-23 16:31:35 +0000

    Refactor create app updated in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,13 @@ from app.models import User
 from werkzeug.security import generate_password_hash
 from app.admin.routes import admin_router
 from app.config import Config
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+
+    return app 
+
+app = create_app()
